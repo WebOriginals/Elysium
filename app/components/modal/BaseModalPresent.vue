f95f190749926db7af7a6c1076b4cd1a7dea4715
Обора Дмитрий <obora_dmitriy@keepcode.ru>
2024-11-12 19:55:07 +0300

    добавил модалку спасибо

--- app/components/modal/BaseModalPresent.vue
+++ app/components/modal/BaseModalPresent.vue
@@ -1,9 +1,10 @@
 <template>
-  <DefaultModal :model-value="true" :title="title" size="l" @update:model-value="(v) => emit('closeModal', v)">
+  <DefaultModal :model-value="true" :title="title" size="l" @update:model-value="(v) => closeModal(v)">
     <template #body>
-      <div class="presentation">
+      <div class="presentation" v-if="!isSubmitted">
         <h2 class="presentation__title">Получите подарок — презентацию "5 ключевых элементов успешного лендинга"!</h2>
-        <p class="presentation__subTitle">Узнайте, как сделать вашу страницу эффективной и привлекать больше участников! Оставьте свои данные, чтобы скачать бесплатную презентацию и применить проверенные методы уже сегодня.</p>
+        <p class="presentation__subTitle">Узнайте, как сделать вашу страницу эффективной и привлекать больше участников!
+          Оставьте свои данные, чтобы скачать бесплатную презентацию и применить проверенные методы уже сегодня.</p>
         <div class="presentation__grid">
           <form @submit.prevent="submitForm" class="presentation__form">
             <UInput
@@ -12,19 +13,42 @@
               color="blue"
               variant="outline"
               size="lg"
-              placeholder="Телефон" />
+              placeholder="Телефон"/>
             <UButton
               type="submit"
               class="submit-button"
               size="xl"
               color="blue"
               block
-            >Получить подарок</UButton>
+            >
+              Получить подарок
+            </UButton>
             <p class="presentation__politic">Нажав на кнопку, я даю согласие на обработку персональных данных</p>
           </form>
           <div class="presentation__image">
-            <img src="/gift.png" alt="Подарок" />
+            <img src="/gift.png" alt="Подарок"/>
           </div>
+        </div>
+      </div>
+      <div class="presentation__thank presentation-thank" v-else>
+        <div class="presentation-thank__text">
+          <h3>Спасибо, что выбрали Elysium!</h3>
+          <p>Ваша презентация уже отправлена, и совсем скоро вы сможете познакомиться с ключевыми элементами создания
+            успешного лендинга. Мы благодарны за ваше доверие и рады поделиться с вами лучшими практиками, которые
+            помогут вывести ваш проект на новый уровень. <br> <br> Если у вас появятся вопросы или понадобится дополнительная
+            помощь — наша команда всегда на связи и готова поддержать вас на каждом этапе. <br> <br> У нас есть еще интересные статьи , можете с ними ознакомиться на
+          <UButton
+            label="Дзен"
+            to="https://dzen.ru/id/66f503af06ec2b139e271289"
+            color="blue"
+            variant="link"
+            size="md"
+            target="_blank"
+            />
+          </p>
+        </div>
+        <div class="presentation__image">
+          <img src="/gift.png" alt="Подарок"/>
         </div>
       </div>
     </template>
@@ -33,8 +57,8 @@
 
 <script setup>
 import DefaultModal from '~/components/modal/DefaultModal.vue';
-import { ref } from 'vue'
-import { useFetch } from '#app'
+import {ref} from 'vue'
+import {useFetch} from '#app'
 
 const emit = defineEmits()
 
@@ -54,55 +78,90 @@ const submitForm = async () => {
       method: 'POST',
       body: {
         chat_id: '931622387',
-        text: `ОПА СКАЧАЛИ ПРЕЗЕНТАЦИЮ \n"5 ключевых элементов успешного лендинга для мероприятий" \nНомер телефона: ${phoneNumber.value}`
+        text: `ОПА СКАЧАЛИ ПРЕЗЕНТАЦИЮ \n"5 ключевых элементов успешного лендинга для мероприятий" \n \nНомер телефона: ${phoneNumber.value}`
       }
     })
     isSubmitted.value = true
+   setTimeout(() => downloadFile(), 10000)
   } catch (error) {
     console.error('Error sending message to Telegram:', error)
   }
 }
+
+const downloadFile = () => {
+  const link = document.createElement('a')
+  link.href = 'doc/gift.pdf'
+  link.download = 'gift.pdf'
+  document.body.appendChild(link)
+  link.click()
+  document.body.removeChild(link)
+}
+const closeModal = (value) => {
+  isSubmitted.value = false
+  emit('closeModal', value)
+}
 </script>
 
 <style scoped lang="scss">
-.presentation{
-  &__title{
+.presentation {
+  &__title {
     font-size: 24px;
     font-weight: 700;
     margin-bottom: 16px;
   }
-  &__subTitle{
+
+  &__subTitle {
     font-size: 16px;
     margin-bottom: 20px;
   }
-  &__grid{
+
+  &__grid {
     display: grid;
     grid-template-columns: 1fr 1fr;
     gap: 20px;
   }
-  &__form{
+
+  &__form {
     display: grid;
     align-items: center;
     gap: 10px;
     align-content: center;
 
-    .phone-input{
+    .phone-input {
       padding: 10px;
     }
 
-    .submit-button{
+    .submit-button {
 
     }
   }
-  &__image{
-    img{
+
+  &__image {
+    img {
       width: 100%;
       height: auto;
     }
   }
-  &__politic{
+
+  &__politic {
     font-size: 12px;
     color: #9B9B9B;
+  }
+
+  &__thank {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 20px;
+
+    h3 {
+      font-size: 24px;
+      font-weight: 700;
+      margin-bottom: 16px;
+    }
+
+    p {
+      font-size: 16px;
+    }
   }
 }
 </style>
