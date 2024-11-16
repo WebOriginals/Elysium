7d5283d66467d68abfc0e66a68366a1b82763ee9
Обора Дмитрий <obora_dmitriy@keepcode.ru>
2024-11-16 12:40:36 +0300

    убрал рекурсию

--- app/plugins/phoneMask.ts
+++ app/plugins/phoneMask.ts
@@ -2,12 +2,17 @@ import { defineNuxtPlugin } from '#app';
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.directive('phone-mask', {
-    mounted(el: HTMLElement, binding) {
-      let inputEl: HTMLInputElement | null = el instanceof HTMLInputElement ? el : el.querySelector('input');
+    mounted(el: HTMLElement) {
+      const inputEl: HTMLInputElement | null = el instanceof HTMLInputElement ? el : el.querySelector('input');
       if (!inputEl) return;
 
+      let isProcessing = false;
+
       inputEl.addEventListener('input', () => {
-        let value = inputEl!.value.replace(/\D/g, '');
+        if (isProcessing) return;
+        isProcessing = true;
+
+        let value = inputEl.value.replace(/\D/g, '');
         if (value.length > 11) {
           value = value.slice(0, 11);
         }
@@ -20,9 +25,10 @@ export default defineNuxtPlugin((nuxtApp) => {
         } else if (value.length > 8 && value.length <= 11) {
           value = value.replace(/(\d{1})(\d{3})(\d{2})(\d{2})(\d{1,3})/, '+7 ($2) $3-$4-$5');
         }
-        inputEl!.value = value;
-        inputEl!.dispatchEvent(new Event('input'));
-        binding.instance[binding.expression] = value;
+        inputEl.value = value;
+        inputEl.dispatchEvent(new Event('input'));
+
+        isProcessing = false;
       });
     },
     getSSRProps() {
