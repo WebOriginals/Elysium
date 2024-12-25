d63f1413aeececed9ece24ba0e0bd34381cda382
Обора Дмитрий <obora_dmitriy@keepcode.ru>
2024-12-25 22:48:04 +0300

    доделал анимацию с ракетой

--- components/lending/TheBanner.vue
+++ components/lending/TheBanner.vue
@@ -5,63 +5,93 @@
 <template>
   <section class="banner">
     <div class="banner__container">
-
       <div class="banner__content">
         <H1 v-html="$t('Lending.banner.title')"></H1>
         <p>{{$t('Lending.banner.description')}}</p>
-        <UiButtonBlue class="banner__button-request" size="xl" block :label="$t('Buttons.leave_a_request')"/>
-<!--        <UiButtonAnimation class="banner__button-request"/>-->
+        <UiButtonBlue
+          class="banner__button-request"
+          size="xxl"
+          block
+          :label="$t('Buttons.leave_a_request')"
+        />
       </div>
       <div class="banner__animation banner-animation">
-        <UiButtonPlay/>
+        <UiButtonPlay />
         <div class="banner-animation__img">
-          <img class="" src="/img/dollar rocket_3D.png" alt="">
+          <img src="/img/dollar rocket_3D.png" alt="" />
         </div>
-        
       </div>
     </div>
   </section>
 </template>
 
 <style scoped lang="scss">
-.banner{
+.banner {
   @apply bg-sky-950 text-white rounded-3xl overflow-hidden p-5 md:p-12;
 
-  &__container{
+  &__container {
     @apply grid gap-9 md:grid-cols-[410px_1fr] lg:grid-cols-[510px_1fr] xl:grid-cols-[610px_1fr];
   }
 
-  &__content{
+  &__content {
     @apply grid gap-5;
 
-    h1{
+    h1 {
       @apply mb-20;
 
-      :deep(span){
+      :deep(span) {
         @apply text-sky-600;
       }
     }
 
-    p{
+    p {
       @apply text-sky-400 md:w-[340px];
     }
   }
 
-  &__button-request{
+  &__button-request {
     @apply md:w-[340px];
-    //@apply md:w-[380px] md:-mb-11 md:-ml-5;
   }
 
-  &-animation{
+  &-animation {
     @apply grid justify-items-end;
 
-    &__img{
-      @apply transform -rotate-[30deg] -translate-y-4 -translate-x-32 w-[100px] scale-[2] md:translate-y-12 md:-translate-x-4 md:w-[200px] md:scale-[1.4] lg:scale-[1.9]   xl:translate-y-8 xl:-translate-x-24;
+    &__img {
+      @apply opacity-100 transform -rotate-[30deg] -translate-y-4 -translate-x-32 w-[100px] scale-[2] md:translate-y-12 md:-translate-x-4 md:w-[200px] md:scale-[1.4] lg:scale-[1.9] xl:translate-y-8 xl:-translate-x-24;
 
-      img{
+      @media only screen and (min-width: 1024px) {
+        animation: combined 9s ease-in-out 1 backwards;
+        animation-delay: 5s;
+      }
+
+
+
+      img {
         @apply w-full h-full;
       }
     }
+  }
+}
+
+
+@keyframes combined {
+  0% {
+    opacity: 1;
+  }
+
+  10%{
+    transform: translateX(-96px) translateY(32px) rotate(-30deg) scale(1.9);
+  }
+  70% {
+    transform: translateY(-700px) translateX(-450px) rotate(-30deg) scale(.6);
+    opacity: 1;
+  }
+
+  75% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 0;
   }
 }
 </style>
--- components/ui/ButtonBlue.vue
+++ components/ui/ButtonBlue.vue
@@ -2,31 +2,98 @@
 const props = defineProps({
   label: {
     type: String,
-    required: true
+    required: true,
   },
   size: {
     type: String,
-    default: "xl"
+    default: "xl",
   },
-  ui: {
-    type: Object,
-    default: {rounded: 'rounded-full'}
+  variant: {
+    type: String,
+    default: 'solid',
+  },
+  rounded: {
+    type: String,
+    default: 'rounded-full',
+  },
+  icon: {
+    type: String,
+    default: null,
   },
   block:{
     type: Boolean,
-    default: false
-  }
-
+    default: false,
+  },
 })
+const solid = {
+  disabled: 'disabled:bg-sky-300 disabled:text-sky-400 disabled:opacity-100 dark:disabled:bg-sky-300 ',
+  dark: 'dark:text-red dark:bg-{color}-800 w-full sm:w-auto ',
+  darkFocus: 'dark:focus-visible:outline-{color}-400',
+  darkHover: 'dark:hover:bg-{color}-600',
+  light: 'shadow-sm text-white bg-sky-500 w-full sm:w-auto ',
+  lightFocus:
+    'focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-{color}-500',
+  lightHover: 'hover:bg-white hover:text-black',
+};
+const outline = {
+  disabled:
+    'disabled:bg-sky-300 disabled:text-sky-400 disabled:opacity-100 dark:disabled:bg-sky-300 dark:disabled:border dark:disabled:ring-sky-300 disabled:border disabled:ring-sky-300 dark:disabled:text-sky-400',
+  dark: 'dark:text-sky-800 dark:border-sky-800 dark:bg-transparent',
+  darkFocus: 'dark:focus-visible:border-{color}-400',
+  darkHover: 'dark:hover:bg-{color}-500 dark:hover:text-white dark:hover:border-sky-500',
+  light: 'border border-sky-950 border-current bg-transparent text-sky-950',
+  lightFocus: 'focus-visible:ring-2 focus-visible:border-sky-500',
+  lightHover: 'hover:border-sky-600 hover:bg-sky-600 hover:text-white',
+};
+const soft = {
+  disabled:
+    'dark:disabled:bg-transparent disabled:ring-sky-300 dark:disabled:text-sky-400 disabled:bg-sky-300 disabled:text-sky-400 disabled:opacity-100 dark:disabled:bg-sky-300 ',
+  dark: 'dark:text-white dark:bg-sky-900 ',
+  darkFocus: 'dark:focus-visible:ring-sky-900',
+  darkHover: 'dark:hover:bg-{color}-500 dark:hover:text-white',
+  light: 'text-sky-500 ',
+  lightFocus: 'focus-visible:ring-2 focus-visible:ring-sky-500',
+  lightHover: 'hover:bg-sky-100 hover:text-sky-600',
+};
+const btnStyle = {
+  base: 'focus:outline-none focus-visible:outline-0 disabled:cursor-not-allowed justify-center',
+  size: {
+    '2sm': 'text-sm',
+    sm: 'text-sm',
+    lg: 'text-base',
+    xl: 'text-base',
+    xxl: 'text-2xl',
+  },
+  padding: {
+    '2sm': 'py-0.5 px-0.5',
+    sm: 'py-0.5 px-0.5',
+    md: 'p-1 px-2',
+    lg: 'py-2 px-4',
+    xl: 'py-3 px-6',
+    xxl: 'py-3.5 px-6',
+  },
+  rounded: props.rounded,
+  font: 'font-semibold',
+  variant: {
+    outline: `${outline.light} ${outline.lightFocus} ${outline.lightHover} ${outline.dark} ${outline.darkFocus} ${outline.darkHover} ${outline.disabled}`,
+    solid: `${solid.light} ${solid.lightFocus} ${solid.lightHover} ${solid.dark} ${solid.darkFocus} ${solid.darkHover} ${solid.disabled}`,
+    soft: `${soft.light} ${soft.lightFocus} ${soft.lightHover} ${soft.dark} ${soft.darkFocus} ${soft.darkHover} ${soft.disabled}`,
+  },
+  icon: {
+    base: 'flex-shrink-0',
+    size: {
+      '2xs': 'h-4 w-4',
+      xs: 'h-4 w-4',
+      sm: 'h-4 w-4',
+      md: 'h-5 w-5',
+      lg: 'h-5 w-5',
+      xl: 'h-6 w-6',
+    },
+  },
+};
 </script>
 
 <template>
-  <UButton class="buttonBlue" :label="label" :block="block" :size="size" :ui="ui"/>
+  <UButton :ui="btnStyle" :label="label" :block="block" :size="size" />
 </template>
 
-<style scoped lang="scss">
-.buttonBlue{
-  @apply bg-[#3D5AF1] text-white h-16 text-2xl;
-
-}
-</style>
