c80802362fccff51206ce170a926774df8c53268
Обора Дмитрий <obora_dmitriy@keepcode.ru>
2024-12-27 17:38:16 +0300

    начал делать портфолио

--- components/lending/ThePortfolio.vue
+++ components/lending/ThePortfolio.vue
@@ -5,12 +5,15 @@
 <template>
   <section class="portfolio">
     <div class="portfolio__wrapper">
-      <h2></h2>
+      <h2>{{ $t('Lending.portfolio.title') }}</h2>
       <div class="portfolio__grid">
-        <div class="portfolio__card portfolio-card">
-          <div class="portfolio-card__cover">
-
+        <div class="portfolio__card portfolio-card" v-for="(item, index) in $tm('Lending.portfolio.items')" :key="$rt(index)">
+          <div class="portfolio-card__cover" :style="{ backgroundImage: `url('${$rt(item.bg_url)}')` }">
+            <div class="portfolio-card__logo" >
+              <img :src="$rt(item.logo_url)" alt="">
+            </div>
           </div>
+          <div class="portfolio-card__description" v-html="$rt(item.description)"></div>
         </div>
       </div>
     </div>
@@ -18,5 +21,30 @@
 </template>
 
 <style scoped lang="scss">
+.portfolio{
 
+  &__grid{
+    @apply grid gap-3.5;
+  }
+
+  &-card{
+    @apply grid grid-cols-2 gap-2;
+
+    &__cover{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      overflow: hidden;
+      border-radius: 24px;
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+    }
+
+    &__description{
+      @apply bg-sky-300 p-8 rounded-3xl;
+    }
+  }
+
+}
 </style>
